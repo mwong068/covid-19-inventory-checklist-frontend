4f6fa7a6c75773d89b9f19856677266f9a06c70f
mwong068 <mwong068@gmail.com>
2020-04-13 16:54:36 -0700

    fixed checkbox not unchecking for sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,25 @@ import React from 'react';
 
 class Signup extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+    handleChange = (event) => {
+        console.log(event.target)
+        this.setState({
+            [event.target.className]: event.target.value
+        })
+    }
+
+    handleCheckbox = (event) => {
+        this.setState({
+            canDeliver: event.target.checked
+        })
+    }
+
     render() {
         return (
             <div>
@@ -9,22 +28,43 @@ class Signup extends React.Component{
                 <form>
                     <label>Name:
                         <br></br>
-                        <input type="text" className="name"></input>
+                        <input type="text" className="name" onChange={this.handleChange}></input>
                     </label>
                     <br></br><br></br>
                     <label>Username:
                         <br></br>
-                        <input type="text" className="username"></input>
+                        <input type="text" className="username" onChange={this.handleChange}></input>
                     </label>
                     <br></br><br></br>
                     <label>Password:
                         <br></br>
-                        <input type="password" className="password"></input>
+                        <input type="password" className="password" onChange={this.handleChange}></input>
+                    </label>
+                    <br></br><br></br>
+                    <label>Location
+                        <br></br>
+                        <input type="text" className="location" onChange={this.handleChange}></input>
                     </label>
                     <br></br><br></br>
                     <label>Family Size:
                         <br></br>
-                        <input type="number" className="familySize"></input>
+                        <select className="familySize" value={this.state.value} onChange={this.handleChange}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6+</option>
+                        </select> 
+                    </label>
+                    <br></br><br></br>
+                    <label>
+                        Can deliver:
+                        <input
+                            className="canDeliver"
+                            type="checkbox"
+                            checked={this.state.canDeliver}
+                            onChange={this.handleCheckbox} />
                     </label>
                     <br></br><br></br>
                     <input type="submit" className="Submit" />
